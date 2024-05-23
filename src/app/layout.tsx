05ade881e8a100d84643a455278d649586859a78
douglas.leite <douglas.leite@fiscaltech.com.br>
2024-05-23 18:29:40 -0300

    First commit - inclusão de TS, Tailwind e Redux no projeto Next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt">
+      <Provider store={store}>
+        <body className={inter.className}>{children}</body>
+      </Provider>
     </html>
   );
 }

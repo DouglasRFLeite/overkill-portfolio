9cd2807fdb68cf2127de7f03685254e49d761e8b
douglas.leite <douglas.leite@fiscaltech.com.br>
2024-05-24 17:57:32 +0000

    shadcn -> Button

--- src/components/ui/home/Hero.tsx
+++ src/components/ui/home/Hero.tsx
@@ -1,12 +1,14 @@
 import { Spotlight } from "../aceternity/Spotlight";
+import { Button } from "@/components/ui/shadcn/button";
 
 export default function Hero() {
   return (
-    <section className="flex items-center justify-center h-screen">
+    <section className="flex flex-col items-center justify-center h-screen">
       <Spotlight />
       <h2 className="text-5xl font-semibold text-gray-700">
         OLÁ, MEU NOME É DOUGLAS
       </h2>
+      <Button>Contato</Button>
     </section>
   );
 }
